9b8f1c0ba8ff9671d91d743f0ceb3fa1a6885f5c
Anton <anton.sukhovatkin@rmp.uhn.ca>
2023-09-18 13:15:15 -0400

    added pokemon stats

--- src/PokemonCard.tsx
+++ src/PokemonCard.tsx
@@ -4,18 +4,35 @@ import {initTE, Select} from "tw-elements";
 import {destructure} from "@solid-primitives/destructure";
 
 export const PokemonCard = (props: PokemonItem) => {
-    const {sprite, hp, attack, defense, speed, move} = destructure(props);
+    const {
+        sprite,
+        hp,
+        attack,
+        defense,
+        speed,
+        move
+    } = destructure(props);
     onMount(() => {
         initTE({ Select });
     });
     return (
         <div
-            class="block max-w-[18rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-            <div class="relative overflow-hidden bg-cover bg-no-repeat">
+            class="block relative max-w-[18rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 top-[10px]">
+
+            <div class="relative overflow-hidden bg-cover bg-no-repeat top-[10px]">
                 <img
                     class="rounded-t-lg mx-auto"
                     src={sprite()}
                     alt="" />
+            </div>
+            <div class="relative left-[10px] top-[10px]">
+                Attack: <b>{attack()}</b>
+            </div>
+            <div class="relative left-[10px] top-[10px]">
+                Defense: <b>{defense()}</b>
+            </div>
+            <div class="relative left-[10px] top-[10px]">
+                Speed: <b>{speed()}</b>
             </div>
             <div class="p-6">
                 <div class="w-full bg-neutral-200 dark:bg-neutral-600">
